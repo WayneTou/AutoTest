4e25330c72823f29bac5d5e237f8eff0cd53ad05
WayneTou <dyw2009@126.com>
2021-07-28 11:43:02 +0800

    5

--- pytest_project/debug_test.py
+++ pytest_project/debug_test.py
@@ -1,27 +1,29 @@
+import datetime
 import shutil
 import time
 from tools.send_email import send_email
 from config.settings import BASE_DIR
 import pytest
 import os
+import pytz
 
 if __name__ == '__main__':
     pytest.main(['-v', '-s', os.path.join(BASE_DIR, r'testcase/test_demo/test_demo.py'),
                 '--html', os.path.join(BASE_DIR, r'report/autotest_report.html')])
     # send_email(r'/home/runner/work/AutoTest/AutoTest/pytest_project/report/autotest_report.html')
-    cur_localtime = time.localtime()
-    cur_time = time.strftime(r'%Y-%m-%d %H:%M', cur_localtime)
+    tz = pytz.timezone('Asia/Shanghai')
+    subject_time = datetime.datetime.now(tz).strftime(r"%Y-%m-%d %H:%M")
+    zip_file_time = datetime.datetime.now(tz).strftime(r"%Y%m%d%H%M")
 
     db_folder = os.path.join(BASE_DIR, r'report')
     if not os.path.exists(db_folder):
         os.makedirs(db_folder)
-    zip_file_name = 'AutomationTestReport%s' % time.strftime(
-        r'%Y%m%d%H%M', cur_localtime)
+    zip_file_name = 'AutomationTestReport%s' % zip_file_time
     att_file_path = os.path.join(BASE_DIR, 'data', zip_file_name)
     shutil.make_archive(att_file_path, 'zip', db_folder)
 
     receive = ['wdou@deloitte.com.cn', 'dou_wayneyuewei@126.com']
-    subject = 'Automation Test Execution %s' % cur_time
+    subject = 'Automation Test Execution %s' % subject_time
     with open(os.path.join(BASE_DIR, r'tools/email_demo.html'), 'r', encoding='utf-8') as file:
         content = file.read()
     file_path = att_file_path+'.zip'
--- pytest_project/test.py
+++ pytest_project/test.py
@@ -3,23 +3,28 @@ from tools.send_email import send_email
 from config.settings import BASE_DIR
 import os
 import time
+import pytz
+import datetime
 
 if __name__ == '__main__':
-    cur_time = time.strftime(r'%Y-%m-%d %H:%M', time.localtime())
+    # cur_time = time.strftime(r'%Y-%m-%d %H:%M', time.localtime())
 
-    db_folder = os.path.join(BASE_DIR, r'report')
-    if not os.path.exists(db_folder):
-        os.makedirs(db_folder)
-    zip_file_name = 'AutomationTestReport%s' % time.strftime(
-        r'%Y%m%d%H%M')
-    att_file_path = os.path.join(BASE_DIR, 'data', zip_file_name)
-    shutil.make_archive(att_file_path, 'zip', db_folder)
+    # db_folder = os.path.join(BASE_DIR, r'report')
+    # if not os.path.exists(db_folder):
+    #     os.makedirs(db_folder)
+    # zip_file_name = 'AutomationTestReport%s' % time.strftime(
+    #     r'%Y%m%d%H%M')
+    # att_file_path = os.path.join(BASE_DIR, 'data', zip_file_name)
+    # shutil.make_archive(att_file_path, 'zip', db_folder)
 
-    receive = ['wdou@deloitte.com.cn', 'dou_wayneyuewei@126.com']
-    subject = 'Automation Test Execution %s' % cur_time
-    with open(os.path.join(BASE_DIR, r'tools/email_demo.html'), 'r', encoding='utf-8') as file:
-        content = file.read()
-    file_path = att_file_path+'.zip'
-    file_name = zip_file_name+'.zip'
-    send_email(receive, subject, content, file_path=file_path,
-               file_name=file_name)
+    # receive = ['wdou@deloitte.com.cn', 'dou_wayneyuewei@126.com']
+    # subject = 'Automation Test Execution %s' % cur_time
+    # with open(os.path.join(BASE_DIR, r'tools/email_demo.html'), 'r', encoding='utf-8') as file:
+    #     content = file.read()
+    # file_path = att_file_path+'.zip'
+    # file_name = zip_file_name+'.zip'
+    # send_email(receive, subject, content, file_path=file_path,
+    #            file_name=file_name)
+    tz = pytz.timezone('Asia/Shanghai')
+    cur_localtime = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+    print(cur_localtime)
